de51e7e7e1818ec452e6fa80aab9d896cbce37f0
Tommy Lam <tlam1337@gmail.com>
2020-01-27 17:23:25 -0500

    added a school

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,9 @@
 def draw_school():
-    print("school not found")
+    print("......^......")
+    print("...../.\.....")
+    print("____.....____")
+    print("|...........|")
+    print("|...........|")
+    print("|....____...|")
+    print("|____|..|___|")
     return
