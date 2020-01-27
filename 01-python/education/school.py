748b2dc1a5a260ee482ea19b02237aad59762d36
Tommy Lam <tlam11@bu.edu>
2020-01-27 18:48:16 -0500

    width is variable

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,9 +1,50 @@
-def draw_school():
-    print("......^......")
-    print("...../.\.....")
-    print("____.....____")
-    print("|...........|")
-    print("|...........|")
-    print("|....____...|")
-    print("|____|..|___|")
+def draw_school(length = 0):
+    add_dot(length)
+    print("...^...", end="")
+    add_dot(length)
+    print()
+
+    add_dot(length)
+    print("../.\..", end="")
+    add_dot(length)
+    print()
+
+    add_line(length)
+    print("_....._", end="")
+    add_line(length)
+    print()
+
+    print("|", end="")
+    add_dot(length)
+    print(".....", end="")
+    add_dot(length)
+    print("|")
+
+    print("|", end="")
+    add_dot(length)
+    print(".....", end="")
+    add_dot(length)
+    print("|")
+
+    print("|", end="")
+    add_dot(length)
+    print("_____", end="")
+    add_dot(length)
+    print("|")
+        
+    print("|", end="")
+    add_line(length)
+    print("|...|", end="")
+    add_line(length)
+    print("|")
+    return
+
+def add_dot(length = 0):
+    for i in range(length):
+        print(".", end="")
+    return
+
+def add_line(length = 0):
+    for i in range(length):
+        print("_", end="")
     return
